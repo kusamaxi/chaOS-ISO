cda34b8d52269030ba063e9ab1fdea2602e8c841
Tommi Niemi <tommi@niemi.lol>
2023-04-09 00:24:46 +0300

    make prepare script robust

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
+
+# Log function
+log() {
+	echo "$(date +"%Y-%m-%d %H:%M:%S") - $1"
+}
 
 # Set variables
 mirrorlist_url="https://raw.githubusercontent.com/kusamaxi/chaOS-ISO/main/mirrorlist"
@@ -12,29 +17,30 @@ packages_dest="airootfs/root/packages"
 chaos_skel_dest="airootfs/root/chaos-skel-liveuser"
 config_dest="airootfs/etc/skel/.config/"
 
-# Download mirrorlist for offline installs
+log "Downloading mirrorlist for offline installs"
 wget -qN --show-progress -P "${mirrorlist_dest}" "${mirrorlist_url}"
 
-# Download wallpaper for installed system
+log "Downloading wallpaper for installed system"
 wget -qN --show-progress -P "${wallpaper_dest}" "${wallpaper_url}"
 
-# Make sure build scripts are executable
+log "Making sure build scripts are executable"
 chmod +x "./"{mkarchiso,run_before_squashfs.sh}
 
-# Function to download and set ownership of packages
+log "Function to download and set ownership of packages"
 get_pkg() {
 	sudo pacman -Syw "$1" --noconfirm --cachedir "${packages_dest}" &&
 		sudo chown $USER:$USER "${packages_dest}/"*".pkg.tar"*
 }
 
+log "Getting chaos-skel-xfce4 package"
 get_pkg "chaos-skel-xfce4"
 
-# Build liveuser skel
+log "Building liveuser skel"
 pushd "${chaos_skel_dest}"
 makepkg -f
 popd
 
-# Clone chaOS repository and install config folder content into live system liveuser .config folder
+log "Cloning chaOS repository and installing config folder content into live system liveuser .config folder"
 git clone "${chaos_repo_url}" "/tmp/chaOS"
 cp -r "/tmp/chaOS/config" "${config_dest}"
 rm -rf "/tmp/chaOS"
