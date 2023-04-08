1bc9d2c2ca1e2f1ff949aa55434f441576964fca
Tommi Niemi <tommi@niemi.lol>
2023-04-08 21:17:42 +0300

    rework prepare

--- prepare.sh
+++ prepare.sh
@@ -1,25 +1,40 @@
 #!/usr/bin/env bash
+set -e
 
-# Get mirrorlist for offline installs
-wget -qN --show-progress -P "airootfs/etc/pacman.d/" "https://raw.githubusercontent.com/kusamaxi/chaOS-ISO/main/mirrorlist"
+# Set variables
+mirrorlist_url="https://raw.githubusercontent.com/kusamaxi/chaOS-ISO/main/mirrorlist"
+wallpaper_url="https://raw.githubusercontent.com/kusamaxi/chaos-theming/master/backgrounds/chaos-wallpaper.png"
+chaos_repo_url="https://github.com/kusamaxi/chaOS"
 
-# Get wallpaper for installed system
-wget -qN --show-progress -P "airootfs/root/" "https://raw.githubusercontent.com/kusamaxi/chaos-theming/master/backgrounds/chaos-wallpaper.png"
+mirrorlist_dest="airootfs/etc/pacman.d/"
+wallpaper_dest="airootfs/root/"
+packages_dest="airootfs/root/packages"
+chaos_skel_dest="airootfs/root/chaos-skel-liveuser"
+config_dest="airootfs/etc/skel/.config/"
 
-# current downgrade mesa for calamares lag in vms:
-#wget "https://archive.archlinux.org/packages/m/mesa/mesa-22.1.7-1-x86_64.pkg.tar.zst"
-#mv mesa-22.1.7-1-x86_64.pkg.tar.zst "airootfs/root/packages/"
+# Download mirrorlist for offline installs
+wget -qN --show-progress -P "${mirrorlist_dest}" "${mirrorlist_url}"
+
+# Download wallpaper for installed system
+wget -qN --show-progress -P "${wallpaper_dest}" "${wallpaper_url}"
 
 # Make sure build scripts are executable
-chmod +x "./"{"mkarchiso","run_before_squashfs.sh"}
+chmod +x "./"{mkarchiso,run_before_squashfs.sh}
 
+# Function to download and set ownership of packages
 get_pkg() {
-    sudo pacman -Syw "$1" --noconfirm --cachedir "airootfs/root/packages" \
-    && sudo chown $USER:$USER "airootfs/root/packages/"*".pkg.tar"*
+	sudo pacman -Syw "$1" --noconfirm --cachedir "${packages_dest}" &&
+		sudo chown $USER:$USER "${packages_dest}/"*".pkg.tar"*
 }
 
 get_pkg "chaos-skel-xfce4"
 
 # Build liveuser skel
-cd "airootfs/root/chaos-skel-liveuser"
+pushd "${chaos_skel_dest}"
 makepkg -f
+popd
+
+# Clone chaOS repository and install config folder content into live system liveuser .config folder
+git clone "${chaos_repo_url}" "/tmp/chaOS"
+cp -r "/tmp/chaOS/config" "${config_dest}"
+rm -rf "/tmp/chaOS"
